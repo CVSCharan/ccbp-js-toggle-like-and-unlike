32cb5e9bb7ecd364a452c222dad3a610692f874f
CVSCharan <charan.cvs@gmail.com>
2022-08-10 23:15:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState(
+    "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png"
+  );
+  const [button, setButton] = useState("");
+  const [icon, setIcon] = useState("");
+  const [imageCount, setImageCount] = useState(1);
+  const [buttonCount, setButtonCount] = useState(1);
+  const [iconCount, setIconCount] = useState(1);
+  // const [iconStatus, setIconStatus] = useState(false);
+  // const [buttonStatus, setButtonStatus] = useState(false);
+  // const [imageStatus, setImageStatus] = useState(false);
+
+  function onClickLikeButton() {
+    if (imageCount === 1 && buttonCount === 1 && iconCount === 1) {
+      setImageCount(0);
+      setButtonCount(0);
+      setIconCount(0);
+      setIcon("unlike-icon");
+      setButton("unlike-button");
+      setImage(
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-liked-img.png"
+      );
+    } else {
+      setImageCount(1);
+      setButtonCount(1);
+      setIconCount(1);
+      setImage(
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png"
+      );
+      setIcon("like-icon");
+      setButton("like-button");
+    }
+  }
+  const renderButton = `${button} ml-2`;
+  const renderIcon = `fa fa-thumbs-up ${icon}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div class="app">
+      <img src={image} class="puppy-image" id="puppyImage" alt="img1" />
+      <div class="d-flex flex-row justify-content-center mt-3">
+        <i id="likeIcon" class={renderIcon} aria-hidden="true"></i>
+        <button
+          type="button"
+          id="likeButton"
+          class={renderButton}
+          onClick={onClickLikeButton}
         >
-          Learn React
-        </a>
-      </header>
+          Like
+        </button>
+      </div>
     </div>
   );
 }
